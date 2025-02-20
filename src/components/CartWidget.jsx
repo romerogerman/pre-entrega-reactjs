91052062705eba1e2c8e06b88b830bc973f6bbf3
romerogerman <romerogerman88@gmail.com>
2025-02-19 23:41:19 -0300

    arregle carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,24 @@
-import { FaShoppingCart } from "react-icons/fa";
+import * as React from 'react';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const CartWidget = () => {
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
+export default function CustomizedBadges() {
   return (
-    <div style={{ color: "white", fontSize: "1.5rem", cursor: "pointer" }}>
-      <FaShoppingCart /> <span>3</span>
-    </div>
+    <IconButton aria-label="cart">
+      <StyledBadge badgeContent={4} color="secondary">
+        <ShoppingCartIcon />
+      </StyledBadge>
+    </IconButton>
   );
-};
-
-export default CartWidget;
+}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,4 +1,4 @@
-// src/components/ItemListContainer.js
+/// src/components/ItemListContainer.js
 import React from "react";
 
 const ItemListContainer = ({ greeting }) => {
